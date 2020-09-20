ba4335208a7b87b56fa040df74f1e5145c4cff3f
cmobrien123 <cmobrien123@gmail.com>
2020-09-20 12:49:32 -0400

    Update Ch3Asnt3.1.py

--- Ch3Asnt3.1.py
+++ Ch3Asnt3.1.py
@@ -1,5 +1,8 @@
-# hrs= 45
-# rate = 10.5
+# Exercise 3.1: Rewrite your pay computation to give the employee 1.5 times the
+# rate for hours worked above 40 hours.
+# Enter Hours: 45
+# Enter Rate: 10
+# Pay: 475.0
 
 hrs = input('Enter hours:')
 h = float(hrs)
